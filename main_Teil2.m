5e89848691d306d8ebf388f37a77ca5c46bbc7ea
VL <vl1102@web.de>
2022-04-14 17:16:22 +0200

    VALENTIN: Polygon gefixt + neuer Plot

--- main_Teil2.m
+++ main_Teil2.m
@@ -1,11 +1,7 @@
 clear; clc; % Konsolen Output und Variablen loeschen
 addpath('libs') % Hilfsfunktionen laden
-% addpath('libs/distmesh') % Meshfunktion laden
+addpath('libs/distmesh') % Meshfunktion laden
 %% Polygon 1
-%   Example: (Polygon)
-%      pv=[-0.4 -0.5;0.4 -0.2;0.4 -0.7;1.5 -0.4;0.9 0.1;
-%          1.6 0.8;0.5 0.5;0.2 1;0.1 0.4;-0.7 0.7;-0.4 -0.5];
-%      [p,t]=distmesh2d(@dpoly,@huniform,0.1,[-1,-1; 2,1],pv,pv);
 pv = [-0.4,-0.5;...
        0.4,-0.2;...
        0.4,-0.7;...
@@ -15,12 +11,17 @@ pv = [-0.4,-0.5;...
        0.5, 0.5;...
        0.2, 1  ;...
        0.1, 0.4;...
-      -0.4, 0.7]; % Eckpunkte definieren
+      -0.4, 0.7;... % Anders als im beispiel
+      -0.4,-0.5]; % Eckpunkte definieren
 [vert,tri] = distmesh2d(@dpoly,@huniform,0.1,[-1,-1; 2,1],true,pv,pv); % Gitter erzeugen
 
 %% Polygon 2
-% pv = [0, 0; 0, 22; 24, 30; 24, 22]; % Eckpunkte definieren
-% distmesh2d(@dpoly,@huniform,6,[0,0; 24,30],pv,pv); % Gitter erzeugen %Verusacht endlosschleife
+pv = [ 0,  0;...
+       0, 22;...
+      24, 30;...
+      24, 22;...
+       0,  0]; % Eckpunkte definieren
+distmesh2d(@dpoly,@huniform,6,[0,0; 24,30],true,pv,pv); % Gitter erzeugen %Verusacht endlosschleife
 
 %% Dirichletknoten hinzufuegen und plotten
 dirichlet = (vert(:,1) == -0.4); % Dirichletrand, logischer Vektor
@@ -51,5 +52,11 @@ deformed_area(:,2) = deformed_area(:,2) + V; % Deformierung in x_2 Richtung
 figure() % Neues Fenster erzeugen
 scatter(vert(:,1),vert(:,2),'k','filled'); hold on; % Urspruengliche Flaeche plotten
 scatter(deformed_area(:,1),deformed_area(:,2),46); % Deformierte Flaeche plotten
-% quiver(vert(:,1),vert(:,2),U,V,0) % Berechnetes Vektorfeld (Verschiebung) plotten
+quiver(vert(:,1),vert(:,2),U,V,0) % Berechnetes Vektorfeld (Verschiebung) plotten
 legend("Original","Deformiert","Verschiebung") % Legende hinzufuegen
+
+figure()
+subplot(1,2,1); patch('vertices',vert,'faces',tri,'edgecol','k','facecol',[.8,.9,1]);
+axis equal tight; title("Urspruengliches Gebiet")
+subplot(1,2,2); patch('vertices',deformed_area,'faces',tri,'edgecol','k','facecol',[.8,.9,1]);
+axis equal tight; title("Deformiertes Gebiet")
