fa54e3deb183466606f71098ceaf5342bdabc7a1
VL <vl1102@web.de>
2022-04-19 13:21:03 +0200

    VALENTIN: Weitere Plot Funktionen hinzugefügt

---
+++ libs/plotDeformationVectors.m
@@ -0,0 +1,15 @@
+function [fig,t] = plotDeformationVectors(vert,defVert,U,V,nComparisons,fig)
+if ~(exist('fig','var')) || isempty(fig)
+    fig = figure();
+    t = tiledlayout(1,nComparisons);
+else
+    t = get(fig,'children'); % Extrahiere das TiledLayoutObject unter der Annahme, das alle Plots mit diesem erstellt wurden
+end
+
+nexttile
+scatter(vert(:,1),vert(:,2),'k','filled'); hold on; % Urspruengliche Flaeche plotten
+scatter(defVert(:,1),defVert(:,2),46); % Deformierte Flaeche plotten
+quiver(vert(:,1),vert(:,2),U,V,0) % Berechnetes Vektorfeld (Verschiebung) plotten
+legend("Original","Deformiert","Verschiebung") % Legende hinzufuegen
+end
+
