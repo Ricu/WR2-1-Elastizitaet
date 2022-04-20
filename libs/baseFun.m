3ada899a1cfee5b582637c88cd84c14314572142
ahaende2 <angelina.haendeler@web.de>
2022-04-20 19:22:03 +0200

    Angelina: Basefun ausprobiert Reihenfolge Basisifunktionen

--- libs/baseFun.m
+++ libs/baseFun.m
@@ -7,23 +7,29 @@ if order == 1
                @(x,y) -1,@(x,y) 0, @(x,y) 1};   %dy
 elseif order == 2
     %% P2
+%     vertices = [0,0;
+%                 1,0;
+%                 0,1;
+%                 1/2,0;
+%                 1/2,1/2;
+%                 0,1/2];
     vertices = [0,0;
-                1,0;
                 0,1;
+                1,0;
+                0,1/2;
                 1/2,0;
-                1/2,1/2;
-                0,1/2];
+                1/2,1/2];
     numBaseFun = 6;
     phi = cell(1,numBaseFun);
     % Zeile 1: dx   Zeile 2:  dx
     d_phi = cell(2,numBaseFun);
     A = zeros(numBaseFun);
     A(:,1) = ones(numBaseFun,1);
-    A(:,2) = vertices(:,1);
-    A(:,3) = vertices(:,2);
-    A(:,4) = vertices(:,1).^2;
-    A(:,5) = vertices(:,1).*vertices(:,2);
-    A(:,6) = vertices(:,2).^2;
+    A(:,3) = vertices(:,1);
+    A(:,2) = vertices(:,2);
+    A(:,5) = vertices(:,1).^2;
+    A(:,6) = vertices(:,1).*vertices(:,2);
+    A(:,4) = vertices(:,2).^2;
     for i = 1:numBaseFun
         b = zeros(numBaseFun,1);
         b(i) = 1;
