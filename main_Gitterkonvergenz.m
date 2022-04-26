0cf774c22d3eeb14189771e6b6f9aeb8831a497f
ahaende2 <angelina.haendeler@web.de>
2022-04-26 15:51:39 +0200

    Angelina: neue functions

---
+++ main_Gitterkonvergenz.m
@@ -0,0 +1,46 @@
+clear; clc; % Konsolen Output und Variablen loeschen
+addpath('libs') % Hilfsfunktionen laden
+%% Teste verschiedene Materialparameter
+maxOrder = 2;
+hVec = 1./(2.^(5:6)); % 32, 64
+nu = 0.3;
+nComparisons = maxOrder * length(hVec) * length(nuVec);
+figGrid = figure("Name","Triangulierung",'NumberTitle','off');
+tiledlayout(1,nComparisons);
+figSolution = figure("Name","Loesung des Elastizitaetproblems",'NumberTitle','off');
+tiledlayout(2,nComparisons,TileIndexing='columnmajor');
+figDefVec = figure("Name","Gebietsvergleich: vor und nach Deformation",'NumberTitle','off');
+tiledlayout(1,nComparisons);
+figDefPol = figure("Name","Gebietsvergleich: vor und nach Deformation",'NumberTitle','off');
+tiledlayout(2,nComparisons,TileIndexing='columnmajor');
+%TODO plotfunktionen mehr infos mitgeben
+
+for i = 1:length(nuVec)
+    nu = nuVec(i);
+    for j = 1:length(hVec)
+        h = hVec(j);
+        for order = 1:maxOrder
+            [vert,tri] = genMeshSquare(1,1/h); % TODO val Punkte und Dreiecke erstellen
+            [vert,tri] = extendGridLagr(vert,tri,order);
+            dirichlet = (vert(:,1) == 0); % Dirichletrand, logischer Vektor
+            grid = struct("vert",vert,"tri",tri,"dirichlet",dirichlet); % Gitter in eine Structure  bringen. 
+            plotGridDirichlet(grid,[],figGrid,"Triangulierung der Ordnung 1");
+
+            % PDE
+            E = 210;  % Materialparameter
+            f = @(x,y) [ones(size(x));ones(size(y))]; % Volumenkraft
+            gD = @(x) 0*x; % Dirichlet-Randwertfunktion, x=[x_1;x_2]
+            
+            [U,V] = elastSolver(grid,E,nu,f,gD,order); % Problem loesen
+
+            deformed_area = vert; % Deformierte Liste initialisieren
+            deformed_area(:,1) = deformed_area(:,1) + U; % Deformierung in x_1 Richtung
+            deformed_area(:,2) = deformed_area(:,2) + V; % Deformierung in x_2 Richtung
+            
+            % Plots
+            figSolution = plotVectorfieldSolution(vert,tri,U,V,[],figSolution);
+            figDefVec = plotDeformationVectors(order,vert,deformed_area,U,V,[],figDefVec);
+            figDefPol = plotDeformationPolygons(tri,vert,order,deformed_area,[],figDefPol);
+        end
+    end
+end
