48f5f934ba1551153fb58d773d535ff5f7f327ed
VL <vl1102@web.de>
2022-04-26 16:55:13 +0200

    VALENTIN: EdgeColor entfernt

--- libs/plotVectorfieldSolution.m
+++ libs/plotVectorfieldSolution.m
@@ -9,10 +9,10 @@ end
 
 %subtitle("subtitle")
 nexttile
-trisurf(tri(:,1:3),vert(:,1),vert(:,2),U);
+trisurf(tri(:,1:3),vert(:,1),vert(:,2),U,'EdgeColor','none');
 title("U: x_1 Richtung")
 nexttile
-trisurf(tri(:,1:3),vert(:,1),vert(:,2),V);
+trisurf(tri(:,1:3),vert(:,1),vert(:,2),V,'EdgeColor','none');
 title("V: x_2 Richtung")
 end
 
