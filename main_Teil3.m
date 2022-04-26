8a16c0a07ef7dda1b681391e77a067acb3891fe7
ahaende2 <angelina.haendeler@web.de>
2022-04-26 15:43:14 +0200

    Angelina: neue function für die Tests

--- main_Teil3.m
+++ main_Teil3.m
@@ -13,7 +13,7 @@ figGrid = plotGridDirichlet(grid,2,[],"Triangulierung der Ordnung 1");
 figGrid = plotGridDirichlet(grid2,[],figGrid,"Triangulierung der Ordnung 2");
 %% PDE aus Teil 1
 E = 210; nu = 0.3; % Materialparameter
-f = @(x,y) [ones(size(x));ones(size(y))]; % Volumenkraft
+f = @(x,y) 100*[ones(size(x));zeros(size(y))]; % Volumenkraft
 gD = @(x) 0*x; % Dirichlet-Randwertfunktion, x=[x_1;x_2]
 
 
@@ -41,53 +41,3 @@ deformed_area2(:,2) = deformed_area2(:,2) + V2; % Deformierung in x_2 Richtung
 
 figDefVecPart1 = plotDeformationVectors(2,vert2,deformed_area2,U2,V2,2,figDefVecPart1);
 figDefPolPart1 = plotDeformationPolygons(tri2,vert2,2,deformed_area2,2,figDefPolPart1);
-
-
-%% Teste verschiedene Materialparameter
-maxOrder = 2;
-hVec = 1./(2.^(5:6)); % 32, 64
-nuVec = [0.45, 0.49];%[0.05, 0.2, 0.4, 0.45, 0.49];
-nComparisons = maxOrder * length(hVec) * length(nuVec);
-figGrid = figure("Name","Triangulierung",'NumberTitle','off');
-tiledlayout(1,nComparisons);
-figSolution = figure("Name","Loesung des Elastizitaetproblems",'NumberTitle','off');
-tiledlayout(2,nComparisons,TileIndexing='columnmajor');
-figDefVec = figure("Name","Gebietsvergleich: vor und nach Deformation",'NumberTitle','off');
-tiledlayout(1,nComparisons);
-figDefPol = figure("Name","Gebietsvergleich: vor und nach Deformation",'NumberTitle','off');
-tiledlayout(2,nComparisons,TileIndexing='columnmajor');
-%TODO plotfunktionen mehr infos mitgeben
-
-for i = 1:length(nuVec)
-    nu = nuVec(i);
-    for j = 1:length(hVec)
-        h = hVec(j);
-        for order = 1:maxOrder
-            [vert,tri] = genMeshSquare(1,1/h); % TODO val Punkte und Dreiecke erstellen
-            [vert,tri] = extendGridLagr(vert,tri,2);
-            dirichlet = (vert(:,1) == 0); % Dirichletrand, logischer Vektor
-            grid = struct("vert",vert,"tri",tri,"dirichlet",dirichlet); % Gitter in eine Structure  bringen. 
-            plotGridDirichlet(grid,[],figGrid,"Triangulierung der Ordnung 1");
-
-            % PDE
-            E = 210;  % Materialparameter
-            f = @(x,y) [ones(size(x));ones(size(y))]; % Volumenkraft
-            gD = @(x) 0*x; % Dirichlet-Randwertfunktion, x=[x_1;x_2]
-            
-            [U,V] = elastSolver(grid,E,nu,f,gD,order); % Problem loesen
-
-            deformed_area = vert; % Deformierte Liste initialisieren
-            deformed_area(:,1) = deformed_area(:,1) + U; % Deformierung in x_1 Richtung
-            deformed_area(:,2) = deformed_area(:,2) + V; % Deformierung in x_2 Richtung
-            
-            % Plots
-            figSolution = plotVectorfieldSolution(vert,tri,U,V,[],figSolution);
-            figDefVec = plotDeformationVectors(order,vert,deformed_area,U,V,[],figDefVec);
-            figDefPol = plotDeformationPolygons(tri,vert,order,deformed_area,[],figDefPol);
-        end
-    end
-end
-
-
-
-
