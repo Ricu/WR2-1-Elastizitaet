2fb32b2fccebfd0e4e0c61a22a5a67e91ee3775b
ahaende2 <angelina.haendeler@web.de>
2022-04-29 11:54:45 +0200

    Angelina: neue function fuer Volumenkräfte

--- main_Gitterkonvergenz.m
+++ main_Gitterkonvergenz.m
@@ -2,7 +2,7 @@ clear; clc; % Konsolen Output und Variablen loeschen
 addpath('libs') % Hilfsfunktionen laden
 
 %% Teste verschiedene Gitterfeinheiten
-hVec = 1./(2.^(4:6)); % 1/16, 1/32, 1/64, 1/128
+hVec = 1./(2.^(4:8)); % 1/16, 1/32, 1/64, 1/128
 maxOrder = 2; % Teste verschiedene Ordnungen der Elemente
 
 verts=cell(length(hVec),maxOrder); tris=cell(length(hVec),maxOrder);
@@ -11,7 +11,7 @@ Uref=cell(maxOrder,1); Vref=cell(maxOrder,1);
 for j = 1:length(hVec)
     for order = 1:maxOrder
         % Gittergenerierung
-        [verts{j,order},tris{j,order}] = genMeshSquare(1,1/hVec(j));  %tris{j,order}angulierung mit Eckknoten erstellen
+        [verts{j,order},tris{j,order}] = genMeshSquare(1,1/hVec(j));  % Triangulierung mit Eckknoten erstellen
         [verts{j,order},tris{j,order}] = extendGridLagr(verts{j,order},tris{j,order},order); % Fuer hoehere Ordnung als P1: Hinzufuegen von Knoten
         dirichlet = (verts{j,order}(:,1) == 0); % Dirichletrand, logischer Vektor
         grid = struct("vert",verts{j,order},"tri",tris{j,order},"dirichlet",dirichlet); % Gitter in eine Structure  bringen
@@ -25,16 +25,22 @@ for j = 1:length(hVec)
         [Ucell{j,order},Vcell{j,order}] = elastSolver(grid,E,nu,f,gD,order);
         
         % Die Loesungen der verschiedenen Gitter anpassen, sodass sie nur die
-        % Werte an den uebereinstimmenden Punkten enthalten, also die der groebsten 
+        % Werte an den uebereinstimmenden Punkten enthalten, also die der groebsten
         % Triangulierung
         % sonst: spaeter Dimensionsprobleme beim Vergleich mit der Referenzloesung
         if (j > 1)
-             [ind,loc] = ismember(verts{j,order},verts{1,order},'rows'); % Pruefe, ob Knoten in  beiden Knotenlisten enthalten sind
-             Ucell{j,order}=Ucell{j,order}(ind,:); % Eliminiere Werte an nicht doppelt vorkommende Knoten
-             Vcell{j,order}=Vcell{j,order}(ind,:);
-             Ucell{j,order}(loc(ind),:)=Ucell{j,order}; % Sortiere Werte um entsprechend der Knotensortierung (notwendig fuer Elemente hoeherer Ordnung)
-             Vcell{j,order}(loc(ind),:)=Vcell{j,order};
-       end
+            % Pruefe, ob Knoten in  beiden Knotenlisten enthalten sind
+            % ind: logischer Vektor; loc: gibt Position des Knotens in der anderen Liste an
+            [ind,loc] = ismember(verts{j,order},verts{1,order},'rows'); 
+            
+            % Eliminiere Werte an nicht doppelt vorkommenden Knoten
+            Ucell{j,order}=Ucell{j,order}(ind,:); 
+            Vcell{j,order}=Vcell{j,order}(ind,:);
+            
+            % Sortiere Werte um, sodass die Knotennummerierungen uebereinstimmen (notwendig fuer Elemente hoeherer Ordnung)
+            Ucell{j,order}(loc(ind),:)=Ucell{j,order}; 
+            Vcell{j,order}(loc(ind),:)=Vcell{j,order};
+        end
         
         % Die Loesung auf dem feinsten Gitter dient als Referenzloesung
         if (j == length(hVec))
@@ -53,20 +59,22 @@ for j=1:length(hVec)-1
         Vdiff{j,order}=norm((Vcell{j,order}-Vref{order})',1)/length(Vcell{j,order});
     end
 end
-Udiff=cell2mat(Udiff); Vdiff=cell2mat(Vdiff);
+Udiff=cell2mat(Udiff); Vdiff=cell2mat(Vdiff); % Cell-Array in Matrix umwandeln
 
-% Plotten
-figure("Name","Gitterkonvergenz: Abweichung der Loesung von der Referenzloesung fuer verschiedene Gitterfeinheiten")
+% Plotten der Ergebnisse
+figure("Name","Gitterkonvergenz")
 for order = 1:maxOrder
     subplot(1,maxOrder,order)
-    plot(hVec(1:end-1),Udiff(:,order));
-    hold on;
-    plot(hVec(1:end-1),Vdiff(:,order));
-    xlabel('Gitterfeinheit')
-    ylabel('Abweichung (Absolutbetrag der Differenz der gemittelten Zeilensummennormen)')
+    plot(hVec(1:end-1),Udiff(:,order),'blue'); hold on;
+    plot(hVec(1:end-1),Vdiff(:,order),'red'); hold on;
+    scatter(hVec(1:end-1),Udiff(:,order),'filled','blue'); hold on;
+    scatter(hVec(1:end-1),Vdiff(:,order),'filled','red');
     xlim([0,hVec(1)])
     ylim([0,max(max(Udiff(:,order)),max(Vdiff(:,order)))])
+    xlabel('Gitterfeinheit')
+    ylabel('Abweichung in gemittelter Zeilensummennorm')
     legend('Abweichung in x1-Richtung','Abweichung in x2-Richtung','Location','northwest')
-    title(sprintf('Ordnung %g',order))   
+    title('Abweichung der Loesung von der Referenzloesung')
+    subtitle(sprintf('Ordnung %g',order))
 end
 
