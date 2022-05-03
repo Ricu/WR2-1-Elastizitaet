a187c2c486d05d879b823e51d188f41d4fe825c1
VL <vl1102@web.de>
2022-05-03 12:27:12 +0200

    VALENTIN: Weiter aufgeräumt

--- libs/elastSolver.m
+++ libs/elastSolver.m
@@ -1,35 +1,29 @@
 function [U,V,K,F] = elastSolver(grid,E,nu,f,gD,order)
-%Input: grid Structure mit allen Gitterkomponenten
-%Input: E der Young modulus
-%Input: nu die Poisson ratio
-%Input: f Volumenkraft
-%Input: gD Funktion des Dirichletrandes
-%Input: order Ordnung der Elemente
+% Input: grid Structure mit allen Gitterkomponenten
+% Input: E der Young modulus
+% Input: nu die Poisson ratio
+% Input: f Volumenkraft
+% Input: gD Funktion des Dirichletrandes
+% Input: order Ordnung der Elemente
 
-%Output: U Deformation in x1-Richtung
-%Output: V Deformation in x2-Richtung
+% Output: U Deformation in x1-Richtung
+% Output: V Deformation in x2-Richtung
 
 vert = grid.vert; tri = grid.tri; dirichlet = grid.dirichlet; % Gitterdaten extrahieren
-% Da jeder Knoten 2 Basisfunktionen verwendet, muss dies auch im logischen
-% Vektor beruecksichtigt werden
-ind = [2*find(dirichlet)-1 ; 2*find(dirichlet)]; % Dirichletrand um die 2. Komponente erweitern %TODO zweite komponente
-dirichlet2 = false(2*length(vert),1); % Neuen logischen Vektor erstellen
-dirichlet2(ind) = true; % Dirichletknoten markieren
+dirichlet2 = repelem(dirichlet,2);          % Erweitere den Dirichletrand um die 2. Komponente
 
-[mu,lambda]=enu2lame(E,nu); % Materialparameter extrahieren
+[mu,lambda]=enu2lame(E,nu);                 % Materialparameter extrahieren
 [K,~,F] = elastAssemble(vert',tri',mu,lambda,f,dirichlet2,gD,order); % Assemblierungsroutine aufrufen
 
 
-d = zeros(length(F),1); % Loesungsvektor initialisieren
-d(dirichlet2) = gD(vert(dirichlet2)); % Dirichletwerte eintrage
-d(~dirichlet2) = K\F; % Galerkin System lösen
-U = d(1:2:end); % Loesung in x_1 Richtung extrahieren
-V = d(2:2:end); % Loesung in x_2 Richtung extrahieren
+d = zeros(length(F),1);                     % Loesungsvektor initialisieren
+d(dirichlet2) = gD(vert(dirichlet2));       % Dirichletwerte eintrage
+d(~dirichlet2) = K\F;                       % Galerkin System loesen
+U = d(1:2:end);                             % Loesung in x_1 Richtung extrahieren
+V = d(2:2:end);                             % Loesung in x_2 Richtung extrahieren
 end
 
-% TODO: wozu K_dir und F_dir, werden nicht verwendet, nichtmal
-% zurueckgegeben beim funktionsausfruf
-function [K,M,F,K_dir,F_dir] = elastAssemble(p,t,lambda,mu,f,dirichlet,gD,order)
+function [K,M,F] = elastAssemble(p,t,lambda,mu,f,dirichlet,gD,order)
 % Input: p als matrix mit allen Knoten
 % Input: e als matrix mit allen Kanten
 % Input: t als matrix mit allen Verbindungen der Triangulierung
@@ -38,7 +32,7 @@ function [K,M,F,K_dir,F_dir] = elastAssemble(p,t,lambda,mu,f,dirichlet,gD,order)
 % Output: M als global assemblierte Massenmatrix
 % Output: F als global assemblierter Lastvektor
 
-% Lade Basisfunktionen und Quadraturformeln
+%% Lade Basisfunktionen und Quadraturformeln
 [phihat,d_phihat] = baseFun(order); %TODO: variable order, statt hard coding 1
 if order == 1
     quad_low = load("quad_formeln.mat").quadratur_P1;
@@ -48,55 +42,51 @@ elseif order == 2
     quad_high = load("quad_formeln.mat").quadratur_P5;
 end
 
-n_nodes = 2*size(p,2); % absolute Anzahl der Freiheitsgrade entspricht Anzahl an Knoten*2
-F=zeros(n_nodes,1); % initialisiere Lastvektor
 
-% Matrizen vorbereiten, in die die Index-Wertepaare für K&M gespeichert
-% werden
-nBaseFun = 2*length(phihat); % Anzahl Basisfunktion für Order = 1: 6
-% Anzahl Basisfunktion für Order (Lagrange) = k: (k+2)*(k+1);
-nEleLoc = nBaseFun^2; % Anzahl Elemente der lokalen Steifigkeitsmatrix
-K_val = zeros(nEleLoc*length(t),1);
-M_val = K_val;
-iIndex = K_val;
-jIndex = K_val;
 
-node_ind2=zeros(nBaseFun,1); % Initialisiere Vektor, in dem die Knotenindizes gespeichert werden
-for i=1:size(t,2) % Über die Elemente iterieren
-    node_ind=t(1:nBaseFun/2,i); % die zum aktuellen Element gehörenden physikalischen Knoten Indizes
+%% Matrizen vorbereiten, in die die Index-Wertepaare gespeichert werden
+nBaseFun = 2*length(phihat);        % Anzahl Basisfunktion
+nElem = length(t);                  % Anzahl Elemente global
+nValLoc = nBaseFun^2;               % Anzahl Eintraege der lokalen Steifigkeitsmatrix
+K_val = zeros(nValLoc*nElem,1);     % Liste fuer K-Werte
+M_val = K_val;                      % Liste fuer M-Werte
+iIndex = K_val;                     % Liste fuer i-Indizes
+jIndex = K_val;                     % Liste fuer j-Indizes
+n_nodes = 2*size(p,2);              % Anzahl Freiheitsgrade entspricht Anzahl Knoten*2
+F=zeros(n_nodes,1);                 % Initialisiere Lastvektor
+
+
+for i=1:size(t,2)                   % Ueber die Elemente iterieren
+    node_ind=t(1:nBaseFun/2,i);     % Die zum aktuellen Element gehoerenden physikalischen Knoten Indizes
     x=p(1,node_ind); y=p(2,node_ind); % Koordinaten der Knoten
        
-    %Affin lineare Abbildung
-    [B_affmap,d_affmap] = aff_map(x,y);
-    detB_affmap = abs(det(B_affmap));
-    InvB_affmap = B_affmap\eye(size(B_affmap));
+    %% Affin lineare Abbildung
+    [B_affmap,d_affmap] = aff_map(x,y); % Affine Abbildung aufstellen
+    detB_affmap = abs(det(B_affmap));   % Determinante der affinen Abb.
+    InvB_affmap = B_affmap\eye(size(B_affmap)); % Inverse der Determinante
     
-    KK=elasticStiffness(lambda,mu,d_phihat,quad_low,nBaseFun,InvB_affmap,detB_affmap); % lokale Steifigkeitsmatrix berechnen 
-    [MK,FK]=elasticMassLoad(phihat,f,B_affmap,d_affmap,detB_affmap,nBaseFun,quad_high); % lokale Massenmatrix und Lastvektor berechnen
+    %% lokale Matrizen aufstellen
+    KK = elasticStiffness(lambda,mu,d_phihat,quad_low,nBaseFun,InvB_affmap,detB_affmap);    % lokale Steifigkeitsmatrix berechnen 
+    [MK,FK] = elasticMassLoad(phihat,f,B_affmap,d_affmap,detB_affmap,nBaseFun,quad_high);   % lokale Massenmatrix und Lastvektor berechnen
     
-    % Assembliere durch addieren der lokalen Matrizen an die richtigen
-    % Stellen der globalen Matrizen
-    node_ind2(2:2:end)=2*node_ind;          % Knoten Indizes um die 2. Komponente erweitern
-    node_ind2(1:2:end)=2*node_ind-1;        % Knoten Indizes um die 2. Komponente erweitern
+    %% Indizes plus zugehoerige Werte an die richtigen Stellen schreiben
+    node_ind2 = 2*repelem(node_ind,2);  % Um die 2. Komponente erweitern
+    node_ind2(1:2:end) = node_ind2(1:2:end) - 1; % Korrigiere Indizes
    
-    iIndex((i-1)*nEleLoc+1:i*nEleLoc) = reshape(repmat(node_ind2',nBaseFun,1),nEleLoc,1); % i-Indizes speichern
-    jIndex((i-1)*nEleLoc+1:i*nEleLoc) = repmat(node_ind2,nBaseFun,1); % j-Indizes speichern
-    K_val((i-1)*nEleLoc+1:i*nEleLoc) = reshape(KK,nEleLoc,1); % Werte für die Steifigkeitsmatrix speichern
-    M_val((i-1)*nEleLoc+1:i*nEleLoc) = reshape(MK,nEleLoc,1); % Werte für die Massenmatrix speichern
-    F(node_ind2) = F(node_ind2) + FK; % Der Lastvektor kann ohne Umwege aktualisiert werden
+    iIndex((i-1)*nValLoc+1:i*nValLoc) = reshape(repmat(node_ind2',nBaseFun,1),nValLoc,1); % i-Indizes speichern
+    jIndex((i-1)*nValLoc+1:i*nValLoc) = repmat(node_ind2,nBaseFun,1); % j-Indizes speichern
+    K_val((i-1)*nValLoc+1:i*nValLoc) = reshape(KK,nValLoc,1);   % Werte für die Steifigkeitsmatrix speichern
+    M_val((i-1)*nValLoc+1:i*nValLoc) = reshape(MK,nValLoc,1);   % Werte für die Massenmatrix speichern
+    F(node_ind2) = F(node_ind2) + FK;                           % Der Lastvektor kann ohne Umwege aktualisiert werden
 end
+%% Globale Matrizen aufstellen
 K = sparse(iIndex,jIndex,K_val,n_nodes,n_nodes); % Anhand der zuvor erstellten Listen die Steifigkeitsmatrix erstellen
 M = sparse(iIndex,jIndex,M_val,n_nodes,n_nodes); % Anhand der zuvor erstellten Listen die Massenmatrix erstellen
 
 
-K_dir = K; % Speichere die Steifigkeitsmatrix inklusive der Dirichletknoten
-F_dir = F - K(:,dirichlet)* gD(p(dirichlet)); % Speichere den Lastvektor inklusive der Dirichletknoten 
-
-double_points = zeros(2,2*length(p));
-double_points(2:2:end) = p; % TODO Vale: doppelte punkte bessser abspeichern?
-double_points(1:2:end) = p;
+double_points = repelem(p,2,1);             % Knotenliste um 2. Komponente erweitern
 F = F(~dirichlet)- K(~dirichlet,dirichlet)*gD(double_points(dirichlet)); 
-K = K(~dirichlet,~dirichlet); % Dirichletknoten aus der Steifigkeitsmatrix eliminieren
+K = K(~dirichlet,~dirichlet);               % Dirichletknoten aus der Steifigkeitsmatrix eliminieren
 % TODO VAL, muss G_D hier abgezogen werden? Glaube ja.
 
 % Dirichletknoten aus dem Lastvektor eliminieren %TODO
