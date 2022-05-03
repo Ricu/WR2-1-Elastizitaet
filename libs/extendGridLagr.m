964645beee7d57f9cfd9afcd10e81194fcd1f515
VL <vl1102@web.de>
2022-05-03 12:54:54 +0200

    VALENTIN: Funktion aufgehübscht und kommentiert

--- libs/extendGridLagr.m
+++ libs/extendGridLagr.m
@@ -1,50 +1,53 @@
 function [vert,tri] = extendGridLagr(vert,tri,order)
-%extendGridLagr erweitert unser Gitter für P1-Elementen auf P2-Elementen 
-%indem die Kantenmittelpunkte hinzugefuegt werden
+% extendGridLagr erweitert das Gitter fuer P1-Elemente auf P2-Elemente
+% indem die Kantenmittelpunkte hinzugefuegt werden
 
-%Input: vert als Knotenliste
-%Input: tri als Elementliste mit 3 Knoten je Element
-%Input: order als gewuenschte Ordnung der Elemente
+% Input: vert als Knotenliste
+% Input: tri als Elementliste mit 3 Knoten je Element
+% Input: order als gewuenschte Ordnung der Elemente
 
-%Output: vert als erweiterte Knotenliste
-%Output: tri als erweiterte Elementliste mit 6 Knoten je Element
+% Output: vert als erweiterte Knotenliste
+% Output: tri als erweiterte Elementliste mit z.B. 6 Knoten je Element fuer
+%         Order = 1
 
-%EXTENDGRIDLAGR Summary of this function goes here
-%   Detailed explanation goes here
-
-if order == 1
+if order == 1 % Fuer order = 1 gibt es nicht zu tun.
     return
 end
-nElements = size(tri,1);
-vertCounter = size(vert,1);
-nAdd = sum(3:order+1); %Anzahl an Knoten die Pro element hinzugefügt werden
-vertNew = zeros(nAdd*nElements,2); % Neue Knotenliste (inkl. mglicher Duplikate)
-counter = 1;
 
+nElements = size(tri,1);            % Anzahl Elemente
+maxIndOrig = size(vert,1);          % Hoechster urspruenglicher Knotenindex
+nAdd = sum(3:order+1);              % Anzahl an Knoten die pro Element hinzugefuegt werden
+vertNew = zeros(nAdd*nElements,2);  % Neue Knotenliste (inkl. moeglicher Duplikate)
+counter = 1;                        % Zaehler
+
+% Idee: berechne fuer jedes Element die Knotenmittelpunkte und eliminiere
+% anschliessend die doppelt berechnenten Knoten. Mittels eines dabei
+% entstehenden Abbildungsvektors lassen sich die Elementknoten einfach
+% zuordnen. Berechne dies alles fuer die neuen Knoten und fuege die neue
+% Information an die alten an.
 for i = 1:nElements
-    x = vert(tri(i,:),1);
-    y = vert(tri(i,:),2);
-    window = counter:counter+nAdd-1; %window entspricht unserer betrachteten Indexmenge
-    newValues = zeros(nAdd,2);
-%     for j = 1:order-1
-%         
-%     end
-%     scatter(newValues(:,1),newValues(:,2))
-    vertNew(window,1) = 1/2*(x+circshift(x,1)); %circshift verschiebt element um 1
-    vertNew(window,2) = 1/2*(y+circshift(y,1)); %circshift verschiebt element um 1
-    counter = counter + nAdd;
+    x = vert(tri(i,:),1);               % x-Koordinaten der Elementknoten
+    y = vert(tri(i,:),2);               % y-Koordinaten der Elementknoten
+    window = counter:counter+nAdd-1;    % aktuell betrachtete Indizes
+    % Berechne die Knotenmittelpunkte in x und y Komponente mittels circshift
+    vertNew(window,1) = 1/2*(x+circshift(x,1)); % x-Komponente
+    vertNew(window,2) = 1/2*(y+circshift(y,1)); % y-Komponente
+    counter = counter + nAdd;                   % Aktualisiere den Zaehler
 end
 
-[vertNew,~,ic] = unique(vertNew,"rows","stable"); %Entferne die Knoten, 
-% welche mehrfach erstellet wurden ic ist nun eine Abbildung von mehrfachen 
-% Knoten auf die einzigartigen dies kann dann im Folgenden zur Markierung 
-% der Knoten auf die enzelnen Elemente genutzt werden. Praktischerweise 
-% wurden die urspruenglichen Knoten in der richtigen Reihenfolge erstellt 
-% (in dem loop)
+% Entferne die Knoten, welche mehrfach erstellt wurden. 
+% ic bildet die Knotenindizes vor der Eliminierung auf jene danach ab.
+% Dies kann dann im Folgenden zur Markierung der Knoten auf die enzelnen
+% Elemente genutzt werden. Praktischerweise wurden die urspruenglichen
+% Knoten in der richtigen Reihenfolge erstellt 
+[vertNew,~,ic] = unique(vertNew,"rows","stable"); 
 
-% haben bis hier fuer jedes Element 3 neue Knoten erstellt
-triNew = reshape(ic, [nAdd,size(tri,1)])'; %markiere Knoten
-triNew = triNew + vertCounter; %Korrigiere um die bereits bestehenden Knoten
-vert = [vert; vertNew]; % die neuen Knoten an die Knotenliste anfuegen
-tri=[tri,triNew]; % die neuen Elemente an die Elementliste anfuegen
+% Bringe den Abbildungsvektor in die Form fuer die Elementliste
+triNew = reshape(ic, [nAdd,size(tri,1)])'; 
+% Korrigiere den Index um die bereits bestehenden Knoten
+triNew = triNew + maxIndOrig;
+% Fuege die neuen Knoten an die alten an
+vert = [vert; vertNew];
+% Fuege die neue Elementmarkierung an die alte an
+tri=[tri,triNew];
 end
