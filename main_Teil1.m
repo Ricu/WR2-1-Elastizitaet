066493083bd6fe26e1bbd46a374318a5985fdc63
ahaende2 <angelina.haendeler@web.de>
2022-05-04 21:30:35 +0200

    Angelina: Teil 1 Verschönerung

--- main_Teil1.m
+++ main_Teil1.m
@@ -1,29 +1,34 @@
 clear; clc; % Konsolen Output und Variablen loeschen
 addpath('libs') % Hilfsfunktionen laden
 addpath('libs/distmesh') % Meshfunktion laden
+
 %% Mesh
 h = 1/16;
-[vert,tri] = genMeshSquare(1,1/h); % Knoten und Elemente erstellen
+[vert,tri] = genMeshSquare(1,1/h); % Knotenliste und Elementeliste erstellen
 dirichlet = (vert(:,1) == 0); % Dirichletrand, logischer Vektor
-grid = struct("vert",vert,"tri",tri,"dirichlet",dirichlet); % Gitter in eine Structure  bringen. 
-% Macht die uebergabe einfacher und dient als logische Einheit
+% Gitter in eine Structure  bringen
+grid = struct("vert",vert,"tri",tri,"dirichlet",dirichlet);  
+% Macht die Uebergabe einfacher und dient als logische Einheit
 figDir = plotGridDirichlet(grid,1,[],"Triangulierung der Ordnung 1");
 
 %% PDE 
 E = 210; nu = 0.3; % Materialparameter
 f = @(x,y) [ones(size(x));ones(size(y))]; % Volumenkraft
 gD = @(x) 0*x; % Dirichlet-Randwertfunktion, x=[x_1;x_2]
-order = 1;    %Grad der Basisfunktionen festlegen
+order = 1;    % Grad der Basisfunktionen festlegen
 
-[U,V] = elastSolver(grid,E,nu,f,gD,order); % Problem loesen
+%% Problem loesen
+[U,V] = elastSolver(grid,E,nu,f,gD,order);
 
 %% Loesung plotten
 figSolution = plotVectorfieldSolution(vert,tri,U,V,1);
 
 %% Deformierte Flaeche darstellen
 deformed_area = vert; % Deformierte Liste initialisieren
-deformed_area(:,1) = deformed_area(:,1) + U; % Deformierung in x_1 Richtung
-deformed_area(:,2) = deformed_area(:,2) + V; % Deformierung in x_2 Richtung
+deformed_area(:,1) = deformed_area(:,1) + U; % Deformation in x_1 Richtung
+deformed_area(:,2) = deformed_area(:,2) + V; % Deformation in x_2 Richtung
 
+% Plot der veraenderten Position der Knoten mit Verschiebungsvektoren
 [figDefVec,~,defVert] = plotDeformationVectors(vert,U,V,order,1);
-figDefPol = plotDeformationPolygons(vert,tri,deformed_area,order,1);
+% Plot des defomierten Polygons
+figDefPol = plotDeformationPolygons(vert,tri,deformed_area,order,1); 
