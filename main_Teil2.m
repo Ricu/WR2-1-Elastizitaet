a6aedab30d31b8e392bfe18b154a11d8ee4ede63
VL <vl1102@web.de>
2022-05-04 14:55:51 +0200

    VALENTIN: Aufgeräumt und Histogramme hinzugefügt.

--- main_Teil2.m
+++ main_Teil2.m
@@ -14,29 +14,24 @@ pv = [-0.4,-0.5;...
       -0.4, 0.7;...
       -0.4,-0.5]; % Eckpunkte definieren
 bbox = [min(pv(:,1)), min(pv(:,2)); max(pv(:,1)), max(pv(:,2))]; % Beschraenke das Gebiet
-[vert,tri] = distmesh2d(@dpoly,@huniform,0.1,bbox,false,pv,pv); % Gitter erzeugen
+h0 = 0.1;
+[vert,tri] = distmesh2d(@dpoly,@huniform,h0,bbox,true,pv,pv); % Gitter erzeugen
 
-dists1 = zeros(3*length(tri),1); % Vektor mit allen Kantenlaengen initialisieren
-miin1 = zeros(length(tri),1); % Vektor fuer alle minimalen Seitenflaechen
-maax1 = zeros(length(tri),1); % Vektor fuer alle maximalen Seitenflaechen
+edgeLengthsPol1 = zeros(3*length(tri),1); % Vektor mit allen Kantenlaengen initialisieren
 counter = 1;
-counteer = 1;
 for i = 1:length(tri) % Iteriere ueber alle Elemente
     x = vert(tri(i,:),1); % x-Koordinaten des Elements
     y = vert(tri(i,:),2); % y-Koordinaten des Elements
-    dists1(counter:counter+2) = sqrt(abs((x-circshift(x,1)).^2+(y-circshift(y,1)).^2));
+    edgeLengthsPol1(counter:counter+2) = sqrt(abs((x-circshift(x,1)).^2+(y-circshift(y,1)).^2));
     % Bestimme die Laenge der Kanten ueber die euklidische Norm
-    maax1(counteer) = max(dists1(counter:counter+2));
-    miin1(counteer) = min(dists1(counter:counter+2));
     counter = counter + 3;
-    counteer = counteer +1;
 end
-difSeitenlaenge1 = maax1./miin1; % Bestimme das maximale Seitenverhaeltnis 
-% fuer jedes Element der Triangulierung
-max1 = max(difSeitenlaenge1); % Bestimme das maximale Seitenverhaeltnis 
-% ueber alle Elemente
-min1 = min(difSeitenlaenge1); % Bestimme das minimale Seitenverhaeltnis 
-% ueber alle Elemente
+edgeLengthsPol1 = dropDuplicateEdges(edgeLengthsPol1,tri);
+figure('Name',"Kantenlaengenverteilung fuer Polygon 1",NumberTitle="off")
+histogram(edgeLengthsPol1)
+xline(h0, '-r','h_0','LineWidth',2);
+ylabel("Frequenz",FontWeight='bold'); xlabel("Kantenlaenge",FontWeight='bold');
+title(sprintf("Kantenlaengenverteilung fuer Polygon 1 mit h_0 = %.1f", h0))
 
 %% Polygon 2
 pv = [ 0,  0;...
@@ -45,29 +40,24 @@ pv = [ 0,  0;...
       24, 22;...
        0,  0]; % Eckpunkte definieren
 bbox = [min(pv(:,1)), min(pv(:,2)); max(pv(:,1)), max(pv(:,2))]; % Beschraenke das Gebiet
-[vert2,tri2] = distmesh2d(@dpoly,@huniform,9,bbox,true,pv,pv); % Gitter erzeugen
+h0 = 9;
+[vert2,tri2] = distmesh2d(@dpoly,@huniform,h0,bbox,true,pv,pv); % Gitter erzeugen
 
-dists2 = zeros(3*length(tri2),1); % Vektor mit allen Kantenlaengen initialisieren
-miin2 = zeros(length(tri),1); % Vektor fuer alle minimalen Seitenflaechen
-maax2 = zeros(length(tri),1); % Vektor fuer alle maximalen Seitenflaechen
+edgeLengthsPol2 = zeros(3*length(tri2),1); % Vektor mit allen Kantenlaengen initialisieren
 counter = 1;
-counteer = 1;
 for i = 1:length(tri2) % Iteriere ueber alle Elemente
-    x = vert(tri2(i,:),1); % x-Koordinaten des Elements
-    y = vert(tri2(i,:),2); % y-Koordinaten des Elements
-    dists2(counter:counter+2) = sqrt(abs((x-circshift(x,1)).^2+(y-circshift(y,1)).^2));
+    x = vert2(tri2(i,:),1); % x-Koordinaten des Elements
+    y = vert2(tri2(i,:),2); % y-Koordinaten des Elements
+    edgeLengthsPol2(counter:counter+2) = sqrt(abs((x-circshift(x,1)).^2+(y-circshift(y,1)).^2));
     % Bestimme die Laenge der Kanten ueber die euklidische Norm
-    maax2(counteer) = max(dists1(counter:counter+2));
-    miin2(counteer) = min(dists1(counter:counter+2));
     counter = counter + 3;
-    counteer = counteer +1;
 end
-difSeitenlaenge2 = maax2./miin2; % Bestimme das maximale Seitenverhaeltnis 
-% fuer jedes Element der Triangulierung
-max2 = max(difSeitenlaenge2); % Bestimme das maximale Seitenverhaeltnis 
-% ueber alle Elemente
-min2 = min(difSeitenlaenge2); % Bestimme das minimale Seitenverhaeltnis 
-% ueber alle Elemente
+edgeLengthsPol2 = dropDuplicateEdges(edgeLengthsPol2,tri2);
+figure('Name',"Kantenlaengenverteilung fuer Polygon 2",NumberTitle="off")
+histogram(edgeLengthsPol2, [0, 0.7*h0, 0.9*h0,1.1*h0, 1.3*h0, 2*h0])
+xline(h0, '-r','h_0','LineWidth',2);
+ylabel("Frequenz",FontWeight='bold'); xlabel("Kantenlaenge",FontWeight='bold');
+title(sprintf("Kantenlaengenverteilung fuer Polygon 2 mit h_0 = %.1f", h0))
 
 
 %% Dirichletknoten hinzufuegen und plotten
@@ -92,3 +82,11 @@ figSolution = plotVectorfieldSolution(vert,tri,U,V,1);
 %% Deformierte Flaeche darstellen
 [~,~,defVert] = plotDeformationVectors(vert,U,V,order,1);
 plotDeformationPolygons(vert,tri,defVert,order,1);
+
+function edgeLengthList_reduced = dropDuplicateEdges(edgeLengthList,tri)
+tri_circ = circshift(tri,1,2);
+edges = [tri(:), tri_circ(:)];
+edges = sort(edges,2);
+[~, ia, ~] = unique(edges,'rows');
+edgeLengthList_reduced = edgeLengthList(ia);
+end
